9528b09635a56107accd64fa297d68d13e5a47b4
Osama <osamaqarem@gmail.com>
2020-04-16 22:27:09 +0800

    update: mirage logger

--- template/src/services/network/service/mock/mirage.ts
+++ template/src/services/network/service/mock/mirage.ts
@@ -9,12 +9,15 @@ export const makeMirage = () =>
   new Server({
     urlPrefix: BuildConfig.EXAMPLE_API_BASE_URL,
     routes() {
-      this.get(
-        "/200",
-        (): GenericResponse => ({
-          code: 200,
-          description: "OK",
-        })
-      )
+      ; (this.pretender.handledRequest = function (verb, path, request) {
+        console.tron(`${verb}: ${path}`, request)
+      }),
+        this.get(
+          "/200",
+          (): GenericResponse => ({
+            code: 200,
+            description: "OK",
+          })
+        )
     },
   })
