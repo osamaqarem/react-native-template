31ad37b1a1be4a52867a994736d231394817e4c8
Osama Qarem <osamaqarem@gmail.com>
2020-06-06 12:56:01 +0800

    fix(✏️): enable multidex for android update: refactor theme

---
+++ template/src/common/theme/theme.ts
@@ -0,0 +1,36 @@
+
+const palette = {
+  red: "#D52031",
+  lightBlue: "#15AAE1",
+  darkBlue: "#10529F",
+  green: "#01CE7F",
+  black: "#000000",
+  lightBlack: "#1D2226",
+  grey: "#999999",
+  darkGrey: "#666666",
+  lightGrey: "#E2E2E2",
+  white: "#FFFFFF",
+  darkWhite: "#F3F4F9",
+}
+
+export type Theme = typeof theme
+
+export const theme = {
+  primaryDark: palette.lightBlack,
+  primary: palette.red,
+  text: palette.black,
+  typography: {
+    header: {
+      fontSize: 20,
+      fontFamily: "Roboto-Bold"
+    },
+    subheader: {
+      fontSize: 18,
+      fontFamily: "Roboto-Medium"
+    },
+    body: {
+      fontSize: 14,
+      fontFamily: "Roboto-Regular"
+    },
+  }
+}
