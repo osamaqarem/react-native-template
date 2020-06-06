05d569ed47909fcc07435792fc53234726ff0b39
Osama Qarem <osamaqarem@gmail.com>
2020-06-06 17:57:27 +0800

    update(💄): fonts and theme

--- template/src/common/theme/theme.ts
+++ template/src/common/theme/theme.ts
@@ -1,35 +1,49 @@
 
 const palette = {
-  red: "#D52031",
-  lightBlue: "#15AAE1",
-  darkBlue: "#10529F",
-  green: "#01CE7F",
-  black: "#000000",
-  lightBlack: "#1D2226",
-  grey: "#999999",
-  darkGrey: "#666666",
-  lightGrey: "#E2E2E2",
-  white: "#FFFFFF",
-  darkWhite: "#F3F4F9",
+  colors: {
+    red: "#D52031",
+    lightBlue: "#15AAE1",
+    darkBlue: "#10529F",
+    green: "#01CE7F",
+    black: "#000000",
+    lightBlack: "#1D2226",
+    grey: "#999999",
+    darkGrey: "#666666",
+    lightGrey: "#E2E2E2",
+    white: "#FFFFFF",
+    darkWhite: "#F3F4F9",
+  },
+  fonts: {
+    montserrat: {
+      regular: 'montserrat_regular',
+      medium: 'montserrat_medium',
+      bold: 'montserrat_bold',
+    },
+    firasans: {
+      regular: 'firasans_regular',
+      medium: 'firasans_medium',
+      bold: 'firasans_bold',
+    },
+  }
 }
 
 
 export const theme = {
-  primaryDark: palette.lightBlack,
-  primary: palette.red,
-  text: palette.black,
+  primaryDark: palette.colors.lightBlack,
+  primary: palette.colors.red,
+  text: palette.colors.black,
   typography: {
     header: {
       fontSize: 20,
-      fontFamily: "Roboto-Bold"
+      fontFamily: palette.fonts.montserrat.bold
     },
     subheader: {
       fontSize: 18,
-      fontFamily: "Roboto-Medium"
+      fontFamily: palette.fonts.firasans.medium
     },
     body: {
       fontSize: 14,
-      fontFamily: "Roboto-Regular"
+      fontFamily: palette.fonts.firasans.regular
     },
   }
 }
