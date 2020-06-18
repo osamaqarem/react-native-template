3f285eeaaab40d07bb1023190dc38ec36910bf58
Osama Qarem <osamaqarem@gmail.com>
2020-06-18 23:34:31 +0800

    fix(✏️): do not throw inside apisauce transforms

--- template/src/services/network/github/githubService.ts
+++ template/src/services/network/github/githubService.ts
@@ -1,41 +1,65 @@
-import { create } from 'apisauce'
+import { create, ApisauceInstance, ApiResponse } from 'apisauce'
 import BuildConfig from "react-native-config"
 import { RepoReadme } from './models'
 import HttpException from '../../../common/exceptions/HttpException'
 
-// API client instance
-const client = create({
+// Apisauce instance
+const instance = create({
     baseURL: BuildConfig.GITHUB_API_BASE_URL,
     timeout: 30 * 1000,
     headers: {
-        accept: 'application/vnd.github.v3+json'
+        'Accept': 'application/vnd.github.v3+json'
     }
 })
 
-// Logging
-__DEV__ && client.addMonitor(res => console.tron(res))
-
-// By default, apisauce does not throw on failure.
-// For SWR to work, the fetcher needs to throw on failure.
-client.addResponseTransform(response => {
-    if (!response.ok) {
-        const error = new HttpException(response.status || 'unknown', response.problem, response.config?.url || 'unknown', response)
-
-        __DEV__ && console.tron(error)
-
-        throw error
-    }
-})
+type Args = Parameters<ApisauceInstance['get']>
 
 /**
- * API implementation
+ * Wrapper around API instance.
+ * By default, apisauce does not throw on failure.
+ * For SWR to work, the fetcher needs to throw on failure.
  */
+const client = {
+    /**
+     * We're forwarding the types and arguments and then throwing on error.
+     */
+    ...instance,
+    get: async <T>(...args: Args) => {
+        const res = await instance.get(...args) as ApiResponse<T>
+        return util.throwOnError<T>(res)
+    }
+    // override more methods as needed
+}
+
+// Logging
+__DEV__ && client.addMonitor(res => console.log(res))
+
+
+
+// API Paths
 const paths = {
     getRepoReadme: () => "/repos/osamaq/react-native-template/readme",
 }
 
+// API implementation
 const api = {
     getRepoReadme: () => client.get<RepoReadme>(paths.getRepoReadme()),
+}
+
+// API utilities
+const util = {
+    throwOnError: <T>(response: ApiResponse<T>) => {
+        if (!response.ok) {
+            const error = new HttpException(response.status || 'unknown', response.problem, response.config?.url || 'unknown', response)
+
+            __DEV__ && console.log(error)
+
+            throw error
+        } else {
+            // all good!
+            return response.data!
+        }
+    }
 }
 
 export const githubService = {
